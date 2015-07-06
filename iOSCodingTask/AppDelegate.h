0e52ecd80cdca812af231fbf63951e2661f76f01
ahmedelashker <ahmed.elashker@esrinea.com>
2015-07-06 16:55:22 +0200

    Initial Commit

---
+++ iOSCodingTask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSCodingTask
+//
+//  Created by Ahmed Elashker on 7/6/15.
+//  Copyright (c) 2015 Ahmed Elashker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSCodingTask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSCodingTask
+//
+//  Created by Ahmed Elashker on 7/6/15.
+//  Copyright (c) 2015 Ahmed Elashker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
